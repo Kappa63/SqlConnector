1151d29450236aafc3cf65a193560ea56b33e554
Karim Q <kareem.qiblawi@gmail.com>
2021-08-31 00:00:41 +0300

    Update SqlConnector.php

--- SqlConnector.php
+++ SqlConnector.php
@@ -1,20 +1,22 @@
 <?php 
-header('Cache-Control: no-cache, no-store, must-revalidate');
-header('Content-Type: application/json');
-header('Pragma: no-cache');
-header('Expires: 0');
+/*
+Author: Karim Q.
+Date: 6/30/2021
+*/
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Content-Type: application/json");
+header("Pragma: no-cache");
+header("Expires: 0");
 
-ini_set('precision', 17);
-ini_set('serialize_precision', -1);
+ini_set("precision", 17);
+ini_set("serialize_precision", -1);
 
-$SqlC = mysqli_connect("localhost", "uvxwjx2spkehg", "Pcig32478@#", $_GET["Db"]);
+$SqlC = mysqli_connect($_GET["Server"], $_GET["Uid"], $_GET["Pwd"], $_GET["Db"]);
 
-$RetType = $_POST["ResFormat"];
+$RetType = $_POST["ResFormat"]; //Can be assoc, both, or index :: defaults to both ::
 $RetType = $RetType=="assoc"?MYSQLI_ASSOC:($RetType=="index"?MYSQLI_NUM:MYSQLI_BOTH);
-// $Key = $_POST["Key"];
-// $Key = sha1($Key);
 $QueryStr = $_POST["Query"];
-$Replaces = json_decode($_POST["Replace"]);
+$Replaces = json_decode($_POST["Replace"]); // Replaces all '??' in the Query String with this data :: stringified array :: ex: '["R1", "R2"]'
 foreach($Replaces as $Rp)
     $QueryStr = substr_replace($QueryStr, $Rp, strpos($QueryStr, "??"), 2);
 
@@ -33,5 +35,6 @@ else {
             array_push($ReturnData["Data"], $d);
     }
 }
-echo json_encode($ReturnData, JSON_PRETTY_PRINT);
+echo json_encode($ReturnData, JSON_PRETTY_PRINT); /*Form :: {"AffectedRows": [Num rows affected by query :: -1 if failed], 
+                                                             "QueryState": [Whether the Query Failed or Not], "Data": [If Query Returns Data]}*/
 ?>
